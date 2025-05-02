e80a065e2105e93ecb15546ac5d7301fcb808203
Parth Patel <patelsparth00@gmail.com>
2025-05-01 22:37:46 -0700

    Further optimized EMS query by early filtering

--- SQL Queries/ems_response_time_optimized.sql
+++ SQL Queries/ems_response_time_optimized.sql
@@ -1,10 +1,9 @@
 WITH base_data AS (
   SELECT 
     ABS((hour_of_arrival_at_scene * 60 + minute_of_arrival_at_scene) - (hour_of_crash * 60 + minute_of_crash)) AS ems_response_time_min
-  FROM `cmpe180b-project.nhtsa_traffic_fatalities.all_accidents`
+  FROM `cmpe180b-project.nhtsa_traffic_fatalities.accidents_partition`
   WHERE 
-    hour_of_arrival_at_scene < 24
-    AND minute_of_arrival_at_scene < 60
+    year_of_crash = 2020
 ),
 
 stats AS (
---
+++ SQL Queries/ems_response_time_unoptimized.sql
@@ -0,0 +1,22 @@
+WITH
+  base_data AS (
+  SELECT
+    year_of_crash,
+    ABS((hour_of_arrival_at_scene * 60 + minute_of_arrival_at_scene) - (hour_of_crash * 60 + minute_of_crash)) AS ems_response_time_min
+  FROM
+    `cmpe180b-project.nhtsa_traffic_fatalities.accidents` ),
+  stats AS (
+  SELECT
+    AVG(ems_response_time_min) AS mean_ems_response,
+    STDDEV(ems_response_time_min) AS stddev_ems_response
+  FROM
+    base_data )
+SELECT
+  *
+FROM
+  base_data,
+  stats
+WHERE
+  ems_response_time_min BETWEEN (mean_ems_response - 2 * stddev_ems_response)
+  AND (mean_ems_response + 2 * stddev_ems_response)
+  AND year_of_crash = 2020;
